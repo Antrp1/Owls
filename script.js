3dcdd08037c4177b9c2911e4e06dff710a15821d
Anthony Padfield <antrp16@gmail.com>
2023-11-13 23:38:24 -0800

    added files

---
+++ script.js
@@ -0,0 +1,3 @@
+const facts = ["Owls Are Made to Have Flexible Necks", "Their Ears Are Wonky but Effective", "Their Feathers Help Them Fly Silently", "The wingspan of an Owl can range anywhere between 18 to 24 in"];
+
+console.log(facts);
